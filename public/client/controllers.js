dcdb157f9ad213b572750697fa9a805805c5dd5b
Hack Reactor Students <cooperjbrandon@gmail.com>
2013-12-13 20:25:59 -0800

    Sorting search results basically complete

--- public/client/controllers.js
+++ public/client/controllers.js
@@ -10,10 +10,22 @@ angular.module('shortlyApp')
   .then(function (data) {
     console.log(data);
     $scope.links = data.data;
+    // if we understood server, this is what we would do for last visited
+    // for (var i = 0; i < $scope.links.length; i++) {
+    //   $scope.links[i].recentClicks = 0;
+    // }
   });
+  $scope.searchText = '';
+  $scope.predicate = '-created_at';
+
+  // if we understood server, this is what we would do for last visited
+  // $scope.totalClicks = 0;
+  // $scope.updateRecent = function(link) {
+  //   $scope.totalClicks++;
+  //   link.recentClicks = $scope.totalClicks;
+  // };
 })
 .controller('ShortenController', function ($scope, $http) {
-  $scope.name = 'Eric';
   $scope.createLink = function () {
     $http({
       method: 'POST',
